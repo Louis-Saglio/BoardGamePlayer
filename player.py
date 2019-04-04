203ee7e2af4c3742e71d9256d5d2d605356333cc
Louis-Saglio <louis.saglio@sfr.fr>
2019-04-05 01:03:53 +0200

    Use function parameter instead of local constant Use while condition instead of if break combo

--- player.py
+++ player.py
@@ -31,20 +31,17 @@ def build_game_tree_for(game: Game, player, precedent_node: ActionNode = None):
     return precedent_node
 
 
-def play():
-    game = TicTacToe()
+def play(game: Game):
     try:
         node = utils.pickle_load_tictactoe_tree()
     except Exception as e:
         print(e)
         node = build_game_tree_for(game, 0)
-    while True:
+    while node.children:
         node: ActionNode = choice(node.children)
         game.play(node.action)
         print(game)
         print("-" * 30)
-        if not node.children:
-            break
 
 
 if __name__ == "__main__":
