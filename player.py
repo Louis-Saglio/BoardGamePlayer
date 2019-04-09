24c5991c69240ca3eab7187ff247753cf9b41bf0
Louis-Saglio <louis.saglio@sfr.fr>
2019-04-09 09:45:17 +0200

    Add utilities to display nodes in a more human friendly way Rewrite prototype algorithm

--- player.py
+++ player.py
@@ -17,6 +17,52 @@ class ActionNode:
         self.children = []
         if self.father is not None:
             self.father.children.append(self)
+        self.cached_outcome = None
+
+    @property
+    def data(self):
+        return f"{self.outcome}, {self.player}"
+
+    @property
+    def id(self):
+        return str(id(self))[-4:]
+
+    def __str__(self):
+        return "\n".join(
+            (
+                f"father   : {getattr(self.father, 'id', None)}, {getattr(self.father, 'data', None)}",
+                f"self     : {self.id}, {self.data}",
+                "children\n\t",
+            )
+        ) + ("\n\t".join(f"{child.id}, {child.data}" for child in self.children) if self.children else " []")
+
+    def predict_outcome_for(self, player):
+        outcome = Game.Result.NOT_ENDED
+        if self.outcome == Game.Result.NOT_ENDED:
+            all_solved = True
+            leads_to_draw = True
+            for child in self.children:
+                if child.outcome == Game.Result.NOT_ENDED:
+                    all_solved = False
+                if leads_to_draw and child.outcome != Game.Result.DRAW:
+                    leads_to_draw = False
+                if child.player == player and child.outcome == Game.Result.WON:
+                    outcome = Game.Result.WON
+                elif child.player != player and child.outcome == Game.Result.LOST:
+                    outcome = Game.Result.LOST
+            if leads_to_draw:
+                outcome = Game.Result.DRAW
+            elif all_solved:
+                outcome = child.outcome
+        # if outcome != Game.Result.NOT_ENDED:
+        #     for child in self.children[:]:
+        #         if child.outcome == outcome:
+                    # print("kill")
+                    # self.children.remove(child)
+                    # del child
+        self.cached_outcome = outcome
+        return outcome
+        # assert node.outcome != Game.Result.NOT_ENDED
 
     @property
     def is_solvable(self):
@@ -32,7 +78,7 @@ class ActionNode:
 def build_game_tree_for(game: Game, player, precedent_node: ActionNode = None):
     assert player in game.players
     if precedent_node is None:
-        precedent_node = ActionNode(player, None, game.get_result_for(player), None)
+        precedent_node = ActionNode(None, None, game.get_result_for(player), None)
     for action in game.get_possible_actions():
         copied_game = game.deepcopy()
         copied_game.play(action)
@@ -42,41 +88,32 @@ def build_game_tree_for(game: Game, player, precedent_node: ActionNode = None):
     return precedent_node
 
 
-def analyse_node_for(node: ActionNode, player):
-    if node.outcome == Game.Result.NOT_ENDED:
-        leads_to_draw = True
-        for child in node.children:
-            if leads_to_draw and child.outcome != Game.Result.DRAW:
-                leads_to_draw = False
-            if child.player == player and child.outcome == Game.Result.WON:
-                node.outcome = Game.Result.WON
-                break
-            elif child.player != player and child.outcome == Game.Result.LOST:
-                node.outcome = Game.Result.LOST
-                break
-        if leads_to_draw:
-            node.outcome = Game.Result.DRAW
-        if node.outcome != Game.Result.NOT_ENDED:
-            for child in node.children[:]:
-                if child.outcome == node.outcome:
-                    # print("kill")
-                    node.children.remove(child)
-                    del child
-
-
 def analyse_tree_for(node: ActionNode, player):
-    if node.outcome != Game.Result.NOT_ENDED:
-        # print(1)
-        pass
-    elif node.is_solvable:
-        # print(2)
-        analyse_node_for(node, player)
-        assert node.outcome != Game.Result.NOT_ENDED
-        # print(node.outcome)
-    else:
-        # print(3)
-        for child in node.children:
-            analyse_tree_for(child, player)
+    i = 0
+    node = node.get_first_final_child()
+    while True:
+        # if i % 1_000_000 == 0:
+        #     print(i)
+        # print("-" * 30)
+        # print(node)
+        # input()
+        if node.outcome != Game.Result.NOT_ENDED:
+            if node.father is None:
+                # print("last node solved")
+                break
+            # print("node is solved, go to father")
+            node = node.father
+        elif node.predict_outcome_for(player) != Game.Result.NOT_ENDED:
+            node.outcome = node.cached_outcome
+            i += 1
+            # print("node is solvable, solve it")
+        else:
+            # print("choose child to solve")
+            for child in node.children:
+                if child.outcome == Game.Result.NOT_ENDED:
+                    # print("solve child")
+                    break
+            node = child
 
 
 def get_game_tree(game) -> ActionNode:
@@ -90,14 +127,37 @@ def get_game_tree(game) -> ActionNode:
 
 def play(game: Game):
     node = get_game_tree(game)
+    print("tree loaded")
     analyse_tree_for(node, game.players[0])
     while node.children:
-        node: ActionNode = choice(node.children)
+        if node.player == game.players[0]:
+            for child in node.children:
+                if child.outcome == Game.Result.WON:
+                    node = child
+                    break
+            else:
+                for child in node.children:
+                    if node.outcome == Game.Result.DRAW:
+                        node = child
+                        break
+                else:
+                    node = node.children[0]
+        else:
+            while True:
+                action = tuple(map(int, input("choose position x,y").split(',')))
+                found = False
+                for child in node.children:
+                    if child.action == action:
+                        found = True
+                        break
+                if found:
+                    break
+            node: ActionNode = choice([child for child in node.children if child.outcome != Game.Result.NOT_ENDED])
+
         game.play(node.action)
         print(game)
         print("result ", node.outcome)
-        # analyse_node_for(node, game.players[0])
-        # print("predict", node.outcome)
+        # print("predict", node.predict_outcome_for(game.players[0]))
         print("-" * 30)
 
 
